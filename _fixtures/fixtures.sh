0a2bf8e529198efef753fd79f1c382974708182b
Josh Mervine <joshua@mervine.net>
2015-09-02 15:15:59 -0700

    adding initial envcfg command pass, needs work, please help

---
+++ _fixtures/fixtures.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "F_STRING    $F_STRING"
+echo "F_INT       $F_INT"
+echo "F_INT32     $F_INT32"
+echo "F_INT64     $F_INT64"
+echo "F_UINT      $F_UINT"
+echo "F_UINT64    $F_UINT64"
+echo "F_FLOAT32   $F_FLOAT32"
+echo "F_FLOAT64   $F_FLOAT64"
+echo "F_BOOL      $F_BOOL"
+echo "F_DURATION  $F_DURATION"
